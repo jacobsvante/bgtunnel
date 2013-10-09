d252235e7a2cd575420b1c4a23e56cd2c1b4604a
Jacob Magnusson <m@jacobian.se>
2013-10-09 21:08:49 +0200

    Update version no to 0.3.0

--- bgtunnel.py
+++ bgtunnel.py
@@ -66,7 +66,7 @@ try:
 except ImportError:
     from queue import Queue, Empty # py3
 
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 
 # NOTE: Not including `open` in __all__ as doing `from bgtunnel import *`
 #       would replace the builtin.
